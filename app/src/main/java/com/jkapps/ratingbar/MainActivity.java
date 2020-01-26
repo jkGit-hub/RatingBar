6ce8dc3d212579e87bd362f2467bcc36cbea929a
jkGit <jk1013@hotmail.com>
2020-01-26 15:04:49 -0800

    Rating Bar

---
+++ app/src/main/java/com/jkapps/ratingbar/MainActivity.java
@@ -0,0 +1,34 @@
+
+package com.jkapps.ratingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), s + " star", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
